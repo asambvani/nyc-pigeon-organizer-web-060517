bbc9d08c9176692bf377928dbe6685d9589470c6
Alex Sambvani <alexsambvani@gmail.com>
2017-05-30 13:35:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  names_array = []
+  hash = Hash.new
+  data.each do |key1, value1|
+    value1.each do |key2, value2|
+      value2.each do |element|
+        names_array << element
+      end
+    end
+  end
+  names_array.uniq!
+  names_array.each do |name|
+    hash[name] = {}
+      data.each do |key1, value1|
+        hash[name][key1] = []
+        value1.each do |key2, value2|
+          value2.each do |element|
+            if element == name
+              hash[name][key1] << key2.to_s
+            end
+          end
+        end
+      end
+  end
+  return hash
 end
